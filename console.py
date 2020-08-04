46143386fc3b305f07cf6bde161d8e7798738fcc
Jonny Reading <jonnyreading@gmail.com>
2020-08-04 15:29:20 +0100

    finished

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ from models.artist import Artist
 import repositories.artist_repository as artist_repository
 import repositories.album_repository as album_repository
 
+album_repository.delete_all()
 artist_repository.delete_all()
 
 artist1 = Artist("ABBA")
@@ -21,11 +22,21 @@ album_repository.save(album3)
 
 artists = artist_repository.select_all()
 
+album1.title = "Greatest Hits Vol. 2"
+album_repository.update(album1)
+
 found_album = album_repository.select(album2.id)
 print(found_album.artist.__dict__)
 
 found_albums = artist_repository.albums(artist1)
 for album in found_albums:
     print(album.__dict__)
+
+album_repository.delete(album3.id)
+
+albums = album_repository.select_all()
+for album in albums:
+    print(album.__dict__)
+
 
 pdb.set_trace()
