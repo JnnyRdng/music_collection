c4551534f5158563038bfc4b824221bba5318cdc
Jonny Reading <jonnyreading@gmail.com>
2020-08-04 14:49:24 +0100

    can get all artists

--- console.py
+++ console.py
@@ -5,7 +5,13 @@ from models.artist import Artist
 import repositories.artist_repository as artist_repository
 import repositories.album_repository as album_repository
 
+artist_repository.delete_all()
+
 artist1 = Artist("ABBA")
 artist_repository.save(artist1)
+artist2 = Artist("Fleetwood Mac")
+artist_repository.save(artist2)
+
+artists = artist_repository.select_all()
 
 pdb.set_trace()
--- repositories/artist_repository.py
+++ repositories/artist_repository.py
@@ -11,3 +11,19 @@ def save(artist):
     id = results[0]["id"]
     artist.id = id
     return artist
+
+
+def delete_all():
+    sql = "DELETE FROM artists"
+    run_sql(sql)
+
+
+def select_all():
+    artists = []
+    sql = "SELECT * FROM artists"
+    results = run_sql(sql)
+
+    for row in results:
+        artist = Artist(row["name"], row["id"])
+        artists.append(artist)
+    return artists
